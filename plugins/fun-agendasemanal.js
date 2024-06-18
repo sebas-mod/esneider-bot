18c78a16427b23035d729b775c482b7f11c00f8c
sebas-mod <luisrojas30921@gmail.com>
2024-06-18 20:57:35 -0300

    Update fun-agendasemanal.js

--- plugins/fun-agendasemanal.js
+++ plugins/fun-agendasemanal.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn, text, command}) => {
 m.react('✅') 
   const yh = global.agenda;
   const url = yh[Math.floor(Math.random() * yh.length)];
-  conn.sendMessage(m.chat, {image: {url: url}, caption: '*🍒 AGENDA SEMANAL 🍒*'}, {quoted: m});
+  conn.sendMessage(m.chat, {image: {url: url}, caption: '*🐉ＡＧＥＮＤＡ ＳＥＭＡＮＡＬ🐉*'}, {quoted: m});
 };
 handler.command = /^(agendasemanal|agenda)$/i;
 handler.tags = ['internet'];
