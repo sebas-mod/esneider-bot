0909b18be7f1937188036db578e8024fc3c181ec
sebas-mod <luisrojas30921@gmail.com>
2024-06-25 02:23:22 -0300

    Actualizar owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn, text, command}) => {
   const id = text ? text : m.chat;
-  await conn.reply(id, '*𝖠𝖽𝗂𝗈𝗌 hijos de puta no los voy a extrañar 😝🖕🏻 𝙴𝚂𝙽𝙴𝙸𝙳𝙴𝚁-𝙱𝙾𝚃 se va *');
+  await conn.reply(id, '*𝖠𝖽𝗂𝗈𝗌 hijos de puta no los voy a extrañar 😝🖕🏻 𝙴𝚂𝙽𝙴𝙸𝙳𝙴𝚁-𝙱𝙾𝚃 se va*');
   await conn.groupLeave(id);
 };
 handler.command = /^(out|salir|leavegc|leave|salirdelgrupo)$/i;
