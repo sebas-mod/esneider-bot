afd7782a4d9932859a98a8117ff4541f117c5013
sebas-mod <luisrojas30921@gmail.com>
2024-07-20 21:42:39 -0300

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -10,7 +10,7 @@ conn.reply(m.chat, stdout.toString(), m)
 }}
 handler.help = ['update']
 handler.tags = ['owner']
-handler.command = ['update', 'actualizar', 'fix', 'fixed'] 
+handler.command = ['update', 'up'] 
 handler.rowner = true
 
 export default handler
