42ed33b7fe3bfa4f01eee04720cd927a62b35fa3
sebas-mod <luisrojas30921@gmail.com>
2024-07-25 15:59:24 -0300

    Actualizar auto-adm.js

--- plugins/auto-adm.js
+++ plugins/auto-adm.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, isAdmin }) => {
   if (m.fromMe) throw 'Nggk'
-  if (isAdmin) throw `> 𝚈𝙰 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾 𝙼𝙸 𝙲𝚁𝙴𝙰𝙳𝙾𝚁`
+  if (isAdmin) throw `*𝚈𝙰 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾 𝙼𝙸 𝙲𝚁𝙴𝙰𝙳𝙾𝚁*`
   await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")
 }
 handler.command = /^dameadmin|tenerpoder$/i
